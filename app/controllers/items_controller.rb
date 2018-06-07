1d306a7b81b92878dd3be3fa1c660b63aca0a712
y.akamatsu <git config --global user.email>
2018-06-07 12:49:04 +0000

    kadai

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_users = @item.have_users
+  end
+
+  def new
+    @items = []
+
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+
+      results.each do |result|
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
+end
---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  validates :code, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :url, presence: true, length: { maximum: 255 }
+  validates :image_url, presence: true, length: { maximum: 255 }
+  
+  has_many :ownerships
+  has_many :users, through: :ownerships
+  has_many :wants
+  has_many :want_users, through: :wants, class_name: 'User', source: :user
+  has_many :have_users, class_name: 'Have', source: :user
+end
