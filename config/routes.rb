99f4ccafcc65e69232d0b22aead60712e197075e
y.akamatsu <git config --global user.email>
2018-06-08 08:24:56 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
 
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
+  
+  get 'rankings/want', to: 'rankings#want'
+  
 end
