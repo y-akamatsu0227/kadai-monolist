fd9f54ed96b85fa101f5e028f840cee4b380a4ec
y.akamatsu <git config --global user.email>
2018-06-08 06:41:45 +0000

    shin

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,6 @@ class Item < ApplicationRecord
   has_many :users, through: :ownerships
   has_many :wants
   has_many :want_users, through: :wants, class_name: 'User', source: :user
-  has_many :have_users, class_name: 'Have', source: :user
+  has_many :haves, class_name: 'Have', source: :user
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
 end
